7922d49f72893dc4e831f14613235e50873ab3cd
Trevor Seidel <tside777@gmail.com>
2023-04-03 16:34:02 -0400

    add test file

--- putUserCount.py
+++ putUserCount.py
@@ -3,24 +3,8 @@ import boto3
 def lambda_handler(event, context):
 
    client = boto3.resource('dynamodb')
-
-   table = client.Table('Cloud_Resume_User_Count')
-
-   response = table.update_item(
-
-       Key={
-           'id': 1,
-       },
-       UpdateExpression='ADD #userCount :q',
-       ExpressionAttributeNames={
-        '#userCount': 'userCount'
-       },
-       ExpressionAttributeValues={ 
-        ':q': 1 
-       },
-       ReturnValues='UPDATED_NEW'
-   )
-
+   
+   response = update_count(client)
 
    return {
 
@@ -28,3 +12,22 @@ def lambda_handler(event, context):
        'body': response['Attributes']
 
    }
+
+def update_count(dynamodb):
+    table = dynamodb.Table('Cloud_Resume_User_Count')
+
+    response = table.update_item(
+        Key={
+            'id': 1,
+        },
+        UpdateExpression='ADD #userCount :q',
+        ExpressionAttributeNames={
+            '#userCount': 'userCount'
+        },
+        ExpressionAttributeValues={ 
+            ':q': 1 
+        },
+        ReturnValues='UPDATED_NEW'
+    )
+
+    return response
---
+++ testPutUser.py
@@ -0,0 +1,71 @@
+from moto import mock_dynamodb
+import boto3
+import unittest
+
+
+def create_user_count_table(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+
+    table = dynamodb.create_table(
+        TableName='Cloud_Resume_User_Count',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'N'   
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+
+    table.meta.client.get_waiter('table_exists').wait(TableName='Cloud_Resume_User_Count')
+    return table
+
+
+
+@mock_dynamodb
+class TestDatabaseFunctions(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Create database resource and mock table
+        """
+        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        self.table = create_user_count_table(self.dynamodb)
+
+
+    def test_table_exists(self):
+        """
+        Test if our mock table is ready
+        """
+        self.assertIn('Cloud_Resume_User_Count', self.table.name)
+    
+
+    def test_putUserCount(self):
+        """
+        Test if our method to update the count works
+        """
+        from putUserCount import update_count
+        result = update_count(self.dynamodb)
+        self.assertEqual(200, result['ResponseMetadata']['HTTPStatusCode'])
+        self.assertEqual(1, result['Attributes']['userCount'])
+
+
+    def tearDown(self):
+        """
+        Delete database resource and mock table
+        """
+        self.table.delete()
+        self.dynamodb=None
+
+if __name__ == '__main__':
+    unittest.main()
