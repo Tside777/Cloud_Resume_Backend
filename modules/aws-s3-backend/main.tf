fa2e15ebca99352599cf060507994281712af7f4
Trevor Seidel <tside777@gmail.com>
2023-04-19 17:45:05 -0400

    remove acl for s3 backened

--- modules/aws-s3-backend/main.tf
+++ modules/aws-s3-backend/main.tf
@@ -12,17 +12,3 @@ resource "aws_s3_bucket_versioning" "versioning" {
         status = "Enabled"
     }
 }
-
-resource "aws_s3_bucket_acl" "backend_acl" {
-    bucket = aws_s3_bucket.backend_bucket.id
-    acl = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "backend_bucket_public_acces" {
-  bucket = aws_s3_bucket.backend_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
